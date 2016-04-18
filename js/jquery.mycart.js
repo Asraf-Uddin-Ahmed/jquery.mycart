03cd2fdbba5e7990436ef568b02fe52c68b500ec
ratul <13ratul@gmail.com>
2016-04-18 19:17:01 +0600

    Basic functionality of cart added. View page prepared. Design updated for add to cart. cart badge added for showing number of carted product. showing total quantity on badge. initialization process updated. add to cart procedure updated to click event of button. showing cart modal feature activated. cart modal prepared with total and grand total. cart modal is update when quantity is changed. cart modal save feature activated. checkout cart activated. remove product from cart activated auto save activated on cart. save cart button replace by close. cart modal design updated. validation added for checking out from empty product list. code re-factored. data field validation added. my-cart integrated with security device. design updated for no of product in cart badge. product icon animate to cart. Add to Cart activated to device detail page. cart activated in all product showcase pages. design of cart and badge is updated prevent user to type anything in quantity portion of cart for validation purpose problem fixed for binding event multiple times product animation updated cart icon attach on top while sroll down Affixing my cart icon bug fixed. Affix cart icon option added. Class added while affixing cart icon. callback function added for click on add to cart design updated for affixed my cart icon animation added for product adding to cart

---
+++ js/jquery.mycart.js
@@ -0,0 +1,333 @@
+/*
+
+*/
+
+(function ($) {
+
+  "use strict";
+
+  var OptionManager = (function () {
+    var objToReturn = {};
+
+    var defaultOptions = {
+      classCartIcon: 'my-cart-icon',
+      classCartBadge: 'my-cart-badge',
+      affixCartIcon: true,
+      checkoutCart: function(products) { },
+      clickOnAddToCart: function($addTocart) { }
+    };
+
+    var getOptions = function (customOptions) {
+      var options = $.extend({}, defaultOptions);
+      if (typeof customOptions === 'object') {
+        $.extend(options, customOptions);
+      }
+      return options;
+    }
+
+    objToReturn.getOptions = getOptions;
+    return objToReturn;
+  }());
+
+
+  var ProductManager = (function(){
+    var objToReturn = {};
+
+    /*
+    PRIVATE
+    */
+    localStorage.products = localStorage.products ? localStorage.products : "";
+    var getIndexOfProduct = function(id){
+      var productIndex = -1;
+      var products = getAllProducts();
+      $.each(products, function(index, value){
+        if(value.id == id){
+          productIndex = index;
+          return;
+        }
+      });
+      return productIndex;
+    }
+    var setAllProducts = function(products){
+      localStorage.products = JSON.stringify(products);
+    }
+    var addProduct = function(id, name, summary, price, quantity, image) {
+      var products = getAllProducts();
+      products.push({
+        id: id,
+        name: name,
+        summary: summary,
+        price: price,
+        quantity: quantity,
+        image: image
+      });
+      setAllProducts(products);
+    }
+
+    /*
+    PUBLIC
+    */
+    var getAllProducts = function(){
+      try {
+        var products = JSON.parse(localStorage.products);
+        return products;
+      } catch (e) {
+        return [];
+      }
+    }
+    var updatePoduct = function(id, quantity) {
+      var productIndex = getIndexOfProduct(id);
+      if(productIndex < 0){
+        return false;
+      }
+      var products = getAllProducts();
+      products[productIndex].quantity = typeof quantity === "undefined" ? products[productIndex].quantity * 1 + 1 : quantity;
+      setAllProducts(products);
+      return true;
+    }
+    var setProduct = function(id, name, summary, price, quantity, image) {
+      if(typeof id === "undefined"){
+        console.error("id required")
+        return false;
+      }
+      if(typeof name === "undefined"){
+        console.error("name required")
+        return false;
+      }
+      if(typeof image === "undefined"){
+        console.error("image required")
+        return false;
+      }
+      if(!$.isNumeric(price)){
+        console.error("price is not a number")
+        return false;
+      }
+      if(!$.isNumeric(quantity)) {
+        console.error("quantity is not a number");
+        return false;
+      }
+      summary = typeof summary === "undefined" ? "" : summary;
+
+      if(!updatePoduct(id)){
+        addProduct(id, name, summary, price, quantity, image);
+      }
+    }
+    var clearProduct = function(){
+      setAllProducts([]);
+    }
+    var removeProduct = function(id){
+      var products = getAllProducts();
+      products = $.grep(products, function(value, index) {
+        return value.id != id;
+      });
+      setAllProducts(products);
+    }
+    var getTotalQuantityOfProduct = function(){
+      var total = 0;
+      var products = getAllProducts();
+      $.each(products, function(index, value){
+        total += value.quantity * 1;
+      });
+      return total;
+    }
+
+    objToReturn.getAllProducts = getAllProducts;
+    objToReturn.updatePoduct = updatePoduct;
+    objToReturn.setProduct = setProduct;
+    objToReturn.clearProduct = clearProduct;
+    objToReturn.removeProduct = removeProduct;
+    objToReturn.getTotalQuantityOfProduct = getTotalQuantityOfProduct;
+    return objToReturn;
+  }());
+
+
+  var loadMyCartEvent = function(userOptions){
+
+    var options = OptionManager.getOptions(userOptions);
+    var $cartIcon = $("." + options.classCartIcon);
+    var $cartBadge = $("." + options.classCartBadge);
+
+    var idCartModal = 'my-cart-modal';
+    var idCartTable = 'my-cart-table';
+    var classProductQuantity = 'my-product-quantity';
+    var classProductTotal = 'my-product-total';
+    var idGrandTotal = 'my-cart-grand-total';
+    var idCheckoutCart = 'checkout-my-cart';
+    var classProductRemove = 'my-product-remove';
+    var idEmptyCartMessage = 'my-cart-empty-message';
+    var classAffixMyCartIcon = 'my-cart-icon-affix';
+
+    $cartBadge.text(ProductManager.getTotalQuantityOfProduct());
+
+    if(!$("#" + idCartModal).length) {
+      $('body').append(
+        '<div class="modal fade" id="' + idCartModal + '" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">' +
+        '<div class="modal-dialog" role="document">' +
+        '<div class="modal-content">' +
+        '<div class="modal-header">' +
+        '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+        '<h4 class="modal-title" id="myModalLabel"><span class="glyphicon glyphicon-shopping-cart"></span> My Cart</h4>' +
+        '</div>' +
+        '<div class="modal-body">' +
+        '<table class="table table-hover table-responsive" id="' + idCartTable + '"></table>' +
+        '</div>' +
+        '<div class="modal-footer">' +
+        '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
+        '<button type="button" class="btn btn-primary" id="' + idCheckoutCart + '">Checkout</button>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>'
+      );
+    }
+
+    var drawTable = function(){
+      var $cartTable = $("#" + idCartTable);
+      $cartTable.empty();
+      var grandTotal = 0;
+
+      var products = ProductManager.getAllProducts();
+      $.each(products, function(){
+        var total = this.quantity * this.price;
+        grandTotal += total;
+        $cartTable.append(
+          '<tr title="' + this.summary + '" data-id="' + this.id + '" data-price="' + this.price + '">' +
+          '<td class="text-center" style="width: 30px;"><img width="30px" height="30px" src="' + this.image + '"/></td>' +
+          '<td>' + this.name + '</td>' +
+          '<td title="Unit Price">$' + this.price + '</td>' +
+          '<td title="Quantity"><input type="number" min="1" style="width: 70px;" class="' + classProductQuantity + '" value="' + this.quantity + '"/></td>' +
+          '<td title="Total" class="' + classProductTotal + '">$' + total + '</td>' +
+          '<td title="Remove from Cart" class="text-center" style="width: 30px;"><a href="javascript:void(0);" class="btn btn-xs btn-danger ' + classProductRemove + '">X</a></td>' +
+          '</tr>'
+        );
+      });
+
+      $cartTable.append(products.length ?
+        '<tr>' +
+        '<td></td>' +
+        '<td><strong>Total</strong></td>' +
+        '<td></td>' +
+        '<td></td>' +
+        '<td id="' + idGrandTotal + '"><strong>$' + grandTotal + '</strong></td>' +
+        '<td></td>' +
+        '</tr>'
+        : '<div class="alert alert-danger" role="alert" id="' + idEmptyCartMessage + '">Your cart is empty</div>'
+      );
+    }
+    var showModal = function(){
+      drawTable();
+      $("#" + idCartModal).modal('show');
+    }
+    var updateCart = function(){
+      $.each($("." + classProductQuantity), function(){
+        var id = $(this).closest("tr").data("id");
+        ProductManager.updatePoduct(id, $(this).val());
+      });
+    }
+    var showGrandTotal = function(){
+      var grandTotal = 0;
+      $("." + classProductTotal).each(function(){
+        grandTotal += $(this).text().match(/\d+/) * 1;
+      });
+      $("#" + idGrandTotal).text("$" + grandTotal);
+    }
+
+    /*
+    EVENT
+    */
+    if(options.affixCartIcon) {
+      var cartIconBottom = $cartIcon.offset().top * 1 + $cartIcon.css("height").match(/\d+/) * 1;
+      var cartIconPosition = $cartIcon.css('position');
+      $(window).scroll(function () {
+        if ($(window).scrollTop() >= cartIconBottom) {
+          $cartIcon.css('position', 'fixed').css('z-index', '999').addClass(classAffixMyCartIcon);
+        } else {
+          $cartIcon.css('position', cartIconPosition).css('background-color', 'inherit').removeClass(classAffixMyCartIcon);
+        }
+      });
+    }
+
+    $cartIcon.click(showModal);
+
+    $(document).on("input", "." + classProductQuantity, function () {
+      var price = $(this).closest("tr").data("price");
+      var id = $(this).closest("tr").data("id");
+      var quantity = $(this).val();
+
+      $(this).parent("td").next("." + classProductTotal).text("$" + price * quantity);
+      showGrandTotal();
+
+      ProductManager.updatePoduct(id, quantity);
+      $cartBadge.text(ProductManager.getTotalQuantityOfProduct());
+    });
+
+    $(document).on('click', "." + classProductRemove, function(){
+      var $tr = $(this).closest("tr");
+      var id = $tr.data("id");
+      $tr.hide(500, function(){
+        ProductManager.removeProduct(id);
+        drawTable();
+        $cartBadge.text(ProductManager.getTotalQuantityOfProduct());
+      });
+    });
+
+    $("#" + idCheckoutCart).click(function(){
+      var products = ProductManager.getAllProducts();
+      if(!products.length) {
+        $("#" + idEmptyCartMessage).fadeTo('fast', 0.5).fadeTo('fast', 1.0);
+        return ;
+      }
+      updateCart();
+      options.checkoutCart(ProductManager.getAllProducts());
+      ProductManager.clearProduct();
+      $cartBadge.text(ProductManager.getTotalQuantityOfProduct());
+      $("#" + idCartModal).modal("hide");
+    });
+
+    $(document).on('keypress', "." + classProductQuantity, function(evt){
+      if(evt.keyCode == 38 || evt.keyCode == 40){
+        return ;
+      }
+      evt.preventDefault();
+    });
+  }
+
+
+  var MyCart = function (target, userOptions) {
+    /*
+    PRIVATE
+    */
+    var $target = $(target);
+    var options = OptionManager.getOptions(userOptions);
+    var $cartIcon = $("." + options.classCartIcon);
+    var $cartBadge = $("." + options.classCartBadge);
+
+    /*
+    EVENT
+    */
+    $target.click(function(){
+      options.clickOnAddToCart($target);
+
+      var id = $target.data('id');
+      var name = $target.data('name');
+      var summary = $target.data('summary');
+      var price = $target.data('price');
+      var quantity = $target.data('quantity');
+      var image = $target.data('image');
+
+      ProductManager.setProduct(id, name, summary, price, quantity, image);
+      $cartBadge.text(ProductManager.getTotalQuantityOfProduct());
+    });
+
+  }
+
+
+  $.fn.myCart = function (userOptions) {
+    loadMyCartEvent(userOptions);
+    return $.each(this, function () {
+      new MyCart(this, userOptions);
+    });
+  }
+
+
+})(jQuery);
